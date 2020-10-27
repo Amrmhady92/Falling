429e2417c08123b0958dc7f7efcaee3ef7eadbb0
Bengan <58001599+Ben-484@users.noreply.github.com>
2020-10-27 10:58:27 +0100

    + Created scene for testing movement + Added interaction to pick up seeds

---
+++ Assets/Interactions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour
+{
+    public int m_seeds = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && collision.gameObject.tag == "Flower")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("Seeds received");
+            m_seeds++;
+        }
+    }
+}
+
