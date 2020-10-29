a515376793f4265a6a9b79e424b896b7b1770694
AmrMHady92 <amrmhady92@gmail.com>
2020-10-29 07:20:27 +0100

    Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public string fallingScene = "";
+    bool loading = false;
+    public void LoadFallingScene()
+    {
+        if (loading) return;
+        loading = true;
+        SceneManager.LoadScene(fallingScene);
+    }
+
+
+}
