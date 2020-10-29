8917fcf23eb4a5b747b12ba21a739b8d35101e32
Bengan <Bengthagnelius@gmail.com>
2020-10-29 11:52:12 +0100

    Gusts now spawn with particle effects and the speed of the particle effects change depending on gust speed

--- Assets/Scripts/Falling/GustSpawner.cs
+++ Assets/Scripts/Falling/GustSpawner.cs
@@ -12,6 +12,8 @@ public class GustSpawner : MonoBehaviour
     public Vector3 randomDirectionMax;
 
     public WindGust gustPrefab;
+    public List<GameObject> windParticles = new List<GameObject>();
+    private ParticleSystem windParticleSystem;
     Transform playerTransform;
 
     void Start()
@@ -40,7 +42,20 @@ public class GustSpawner : MonoBehaviour
         newGust.gustSpeed = Random.Range(gustSpeedMinMax.x, gustSpeedMinMax.y);
         newGust.gustDirection = new Vector3(Random.Range(randomDirectionMin.x, randomDirectionMax.x), Random.Range(randomDirectionMin.y, randomDirectionMax.y), Random.Range(randomDirectionMin.z, randomDirectionMax.z));
         newGust.transform.parent = transform;
+
+        GameObject newWindParticle = Instantiate(windParticles[Random.Range(0, windParticles.Count)], spawnPoint, Quaternion.LookRotation(newGust.gustDirection));
+        newWindParticle.transform.eulerAngles = new Vector3(
+            newWindParticle.transform.eulerAngles.x,
+            newWindParticle.transform.eulerAngles.y - 90,
+            newWindParticle.transform.eulerAngles.z);
+
+        windParticleSystem = newWindParticle.GetComponent<ParticleSystem>();
+        windParticleSystem.Play();
+        var windparticleVelocity = windParticleSystem.velocityOverLifetime;
+        windparticleVelocity.speedModifier = Random.Range(gustSpeedMinMax.x + 2, gustSpeedMinMax.y + 2) / 10;
+
         Destroy(newGust.gameObject, 5f);
+        Destroy(newWindParticle, 5f);
     }
 
     public void SpawnUpdraft() {
