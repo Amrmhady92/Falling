f89a54544d8aababd6bb940f8e730c1608d421f6
Bengan <Bengthagnelius@gmail.com>
2020-10-30 10:40:03 +0100

    Added script for switching camera postions

---
+++ Assets/Scripts/CameraSwitchScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitchScript : MonoBehaviour
+{
+    [Tooltip("Where the camera will be positioned")]
+    public Vector3 cameraTargetPosition;
+    [Tooltip("The rotation the camera will have")]
+    public Vector3 cameraTargetRotation;
+    [Tooltip("Where the camera will be aimaing at, leave at 0,0,0 if not used")]
+    public Vector3 cameraTargetLookAt; //Leave empty if not used
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Camera.main.transform.position = cameraTargetPosition;
+        Camera.main.transform.eulerAngles = cameraTargetRotation;
+        if (cameraTargetLookAt != new Vector3(0, 0, 0))
+            Camera.main.transform.LookAt(cameraTargetLookAt);
+    }
+}
